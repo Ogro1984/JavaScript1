9164bb889c0abd8cc74a83d12dc698c1f2a34caf
Ogro1984 <gonzalezracigmariano@gmail.com>
2019-01-10 23:32:21 -0300

    mas pruebas
    mas pruebas

---
+++ app.js
@@ -0,0 +1,21 @@
+document.write('esto es para probrar camon every body lets gooo' , c)
+var a,b,c;
+a=2;
+b=888855555;
+c=a+b;
+document.write(c)
+
+function escribirenlaconsola() {
+  console.log('Hola desde la consola')
+
+}
+
+setInterval(escribirenlaconsola,1000)
+
+
+function sumarmierdas(t,r){
+
+  o=t+r;
+
+  return (o);
+}
---
+++ codBase/js/index.js
@@ -0,0 +1,22 @@
+
+var persona ={
+nombre:"",
+ciudad:"",
+cobermed:"",
+peso:0,
+estatura:0,
+fecha_nac:new date(),
+obtener_imc:function(this.peso,this.estatura){
+var imc=this.peso/this.estatura^2,
+
+
+
+}
+
+
+}
+
+
+document.getElementById('boton-perfil').addEventListener("click", function(){
+
+})
